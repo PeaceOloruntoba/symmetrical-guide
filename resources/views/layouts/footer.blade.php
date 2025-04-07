755500c8918993f7170677b3788c3b7a14bdb198
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 05:20:15 +0100

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="bg-green-600 text-white py-4 fixed bottom-0 w-full">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-around items-center">
-        <a href="#" class="hover:underline">Impressum</a>
-        <a href="#" class="hover:underline">Datenschutzerklärung</a>
-        <a href="#" class="hover:underline">Allgemeine Geschäftsbedingungen</a>
+<footer class="bg-green-500 text-white py-2">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-around items-center text-sm">
+        <a href="#" class="hover:underline text-decoration-none text-white">IMPRESSUM</a>
+        <a href="#" class="hover:underline text-decoration-none text-white">ALLGEMEINE GESCHÄFTSBEDINGUNG</a>
+        <a href="#" class="hover:underline text-decoration-none text-white">DATENSCHUTZERKLÄRUNG</a>
     </div>
 </footer>
