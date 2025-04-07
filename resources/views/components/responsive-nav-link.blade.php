425ecb9788f6b54529f344a4f8ce306563bd231b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 02:12:37 +0100

    Update responsive-nav-link.blade.php

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -4,7 +4,7 @@
 $classes = 'block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
 
 $classes = ($active ?? false)
-            ? $classes . ' font-medium text-indigo-500'
+            ? $classes . ' font-medium text-[#5BB85C]'
             : $classes;
 @endphp
 
