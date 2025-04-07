e81274fe1a3acc16073c22b2d45cb04e12c457ad
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 02:22:10 +0100

    Update index.blade.php

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1 class="text-2xl font-semibold text-gray-800 mb-4">Companies</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
         @forelse ($companies as $company)
             <div class="bg-white shadow-md rounded-lg overflow-hidden">
                 <div class="p-4">
