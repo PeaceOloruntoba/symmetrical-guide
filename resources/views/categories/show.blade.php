cbaef3eeab4b2649141de02ca6f7f876f9e37f0b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 13:18:34 +0100

    ggg

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -6,24 +6,24 @@
             <x-back-button />
             <span class="text-2xl font-semibold text-gray-800">{{ $category->name }}</span>
         </div>
-        <div>
+        <div class="flex flex-col gap-2">
             @if ($subcategories->isNotEmpty())
-                <span class="text-xl font-semibold text-gray-700 mb-4">Unterkategorien</span>
-                <div class="flex flex-col gap-6 mb-8">
+                <span class="text-xl font-semibold text-gray-700">Unterkategorien</span>
+                <div class="flex flex-col gap-3">
                     @forelse ($subcategories as $subcategory)
                         <div>
                             <span
-                                class="text-lg font-semibold text-green-600 mb-2 bg-gray-300">{{ $subcategory->name }}</span>
-                            <div class="grid grid-cols-2 gap-4 mt-4">
+                                class="text-lg font-semibold text-green-600 bg-gray-300">{{ $subcategory->name }}</span>
+                            <div class="flex gap-4">
                                 @forelse ($subcategory->products->take(4) as $product)
                                     <x-product-card :product="$product" />
                                 @empty
-                                    <p class="text-gray-500 col-span-2">Keine Produkte in dieser Unterkategorie.</p>
+                                    <p class="text-gray-500">Keine Produkte in dieser Unterkategorie.</p>
                                 @endforelse
                             </div>
                             @if ($subcategory->products->count() > 4)
                                 <a href="{{ route('subcategories.show', $subcategory->id) }}"
-                                    class="inline-block mt-2 text-green-500 hover:underline">Mehr anzeigen</a>
+                                    class="inline-block text-green-500 hover:underline">Mehr anzeigen</a>
                             @endif
                         </div>
                     @empty
@@ -34,7 +34,7 @@
         </div>
         <div>
             @if ($products->isNotEmpty())
-                <h3 class="text-xl font-semibold text-gray-700 mt-8 mb-4">Produkte in {{ $category->name }}</h3>
+                <h3 class="text-xl font-semibold text-gray-700">Produkte in {{ $category->name }}</h3>
                 <div class="flex items-center gap-4">
                     @foreach ($products as $product)
                         <x-product-card :product="$product" />
