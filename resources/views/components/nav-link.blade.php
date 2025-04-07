d8133c977df6e57677db7e2ab12b5a4ba90a38cd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 02:15:05 +0100

    Update nav-link.blade.php

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,10 +1,10 @@
 @props(['active'])
 
 @php
-$classes = 'inline-flex items-center px-1 pt-2.5 border-b-2 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+$classes = 'inline-flex items-center px-1 pt-2.5 border-b-2 text-md font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 
 $classes = ($active ?? false)
-            ? $classes . ' border-indigo-400 text-indigo-700 focus:border-indigo-700'
+            ? $classes . ' border-[#5BB85C] text-[#5BB85C] focus:border-[#5BB85C]'
             : $classes . ' border-transparent';
 @endphp
 
