0988a7ff326518ca10991413c02ebe7b45dda376
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 11:14:04 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -32,7 +33,9 @@ class UserController extends Controller
     public function showCategory(Category $category): View
     {
         $subcategories = Category::where('parent_id', $category->id)->with('products')->get();
-        return view('categories.show', compact('category', 'subcategories'));
+        $categories = Category::where('parent_id', null)->get(); // Fetch main categories for the navbar
+
+        return view('categories.show', compact('category', 'subcategories', 'categories'));
     }
 
     /**
