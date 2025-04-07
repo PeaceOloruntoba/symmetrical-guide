ed2caf06f29d8ee60f3757aec0cb06bf8db17a1e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 04:55:22 +0100

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-green-600 text-white py-4 fixed bottom-0 w-full">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-around items-center">
+        <a href="#" class="hover:underline">Impressum</a>
+        <a href="#" class="hover:underline">Datenschutzerklärung</a>
+        <a href="#" class="hover:underline">Allgemeine Geschäftsbedingungen</a>
+    </div>
+</footer>
