95758ead2ca798bea8da3a51589a0e2405835dce
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 06:09:01 +0100

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,44 +1,73 @@
 <nav class="bg-white shadow py-3">
     <div class="w-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-8">
         <div class="flex items-center">
-            <a href="{{ route('home') }}" class="text-decoration-none text-green-500 text-xl font-semibold">{{ config('app.name', 'Germanware') }}</a>
+            <a href="{{ route('home') }}"
+                class="text-decoration-none text-indigo-600 text-xl font-semibold">{{ config('app.name', 'Germanware') }}</a>
         </div>
 
         <div class="flex items-center w-full gap-8">
-            <form action="{{ route('search.products') }}" method="GET" class="relative rounded-full overflow-hidden border border-green-500 w-full text-sm">
-                <input
-                    type="search"
-                    name="search"
+            <form action="{{ route('search.products') }}" method="GET"
+                class="relative rounded-full overflow-hidden border border-green-500 w-full text-sm">
+                <input type="search" name="search"
                     class="block w-full pl-4 pr-10 py-2 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm rounded-l-full"
-                    placeholder="Nach was suchst du ?"
-                >
-                <button
-                    type="submit"
-                    class="absolute inset-y-0 right-0 pr-3 flex items-center bg-green-500 text-white rounded-r-full px-4 focus:outline-none"
-                >
+                    placeholder="Nach was suchst du ?">
+                <button type="submit"
+                    class="absolute inset-y-0 right-0 pr-3 flex items-center bg-green-500 text-white rounded-r-full px-4 focus:outline-none">
                     Suche
                     <svg class="h-5 w-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                     </svg>
                 </button>
             </form>
             <div class="ml-4 relative">
-                <button id="categoryDropdownButton" data-dropdown-toggle="categoryDropdown" class="inline-flex items-center text-gray-700 hover:text-green-500 font-medium text-nowrap">
-                    Alle Kategorien
+                <button id="categoryDropdownButton" data-dropdown-toggle="categoryDropdown"
+                    class="inline-flex items-center text-gray-700 hover:text-green-500 font-medium text-nowrap">
+                    {{ request()->routeIs('categories.index') ? 'Alle Kategorien' : (isset($category) ? $category->name : 'Kategorien') }}
                     <svg class="h-5 w-5 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                     </svg>
                 </button>
-                <div id="categoryDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 shadow dark:bg-gray-700 absolute right-0">
-                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 text-nowrap" aria-labelledby="categoryDropdownButton">
-                        @foreach ($categories as $category)
+                <div id="categoryDropdown"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 shadow dark:bg-gray-700 absolute right-0">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 text-nowrap"
+                        aria-labelledby="categoryDropdownButton">
+                        @if (request()->routeIs('categories.index'))
+                            @foreach ($categories as $category)
+                                <li>
+                                    <a href="{{ route('categories.show', $category->id) }}"
+                                        class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->name }}</a>
+                                </li>
+                            @endforeach
+                        @elseif (isset($subcategories) && $subcategories->isNotEmpty())
+                            @foreach ($subcategories as $subcategory)
+                                <li>
+                                    <a href="{{ route('subcategories.show', $subcategory->id) }}"
+                                        class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $subcategory->name }}</a>
+                                </li>
+                            @endforeach
+                        @else
                             <li>
-                                <a href="{{ route('categories.show', $category->id) }}" class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->name }}</a>
+                                <a href="{{ route('categories.index') }}"
+                                    class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Alle
+                                    Kategorien</a>
                             </li>
-                        @endforeach
-                        <li>
-                            <a href="{{ route('categories.index') }}" class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Alle Anzeigen</a>
-                        </li>
+                        @endif
+                        @if (request()->routeIs('/'))
+                            @foreach ($categories as $category)
+                                <li>
+                                    <a href="{{ route('categories.show', $category->id) }}"
+                                        class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->name }}</a>
+                                </li>
+                            @endforeach
+                        @endif
+                        @if (request()->routeIs('categories.index'))
+                            <li>
+                                <a href="{{ route('home') }}"
+                                    class="block p-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Zurück
+                                    zur Startseite</a>
+                            </li>
+                        @endif
                     </ul>
                 </div>
             </div>
