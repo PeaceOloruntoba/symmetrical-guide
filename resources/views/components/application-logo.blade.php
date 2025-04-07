1165c377c020d59ccc61761375ab991c6ace690a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 02:16:34 +0100

    Update application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/your-logo.png') }}" alt="{{ config('app.name') }}" class="h-9 w-auto">
+<img src="{{ asset('images/your-logo.png') }}" alt="{{ config('app.name') }}" class="h-9 w-auto text-[#5BB85C] font-bold">
