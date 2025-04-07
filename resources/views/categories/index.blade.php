bca26f1a41dcbc350ef5cd52c0c1798742242558
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 06:36:12 +0100

    Update index.blade.php

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,12 +4,17 @@
     <div class="py-6">
         <x-back-button />
         <h2 class="text-2xl font-semibold text-gray-800 mb-4">Alle Kategorien</h2>
-        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <div class="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
             @foreach ($categories as $category)
-                <a href="{{ route('categories.show', $category->id) }}" class="bg-white rounded-lg shadow-md p-4 text-center hover:shadow-lg transition duration-200">
-                    <h3 class="text-lg font-semibold text-indigo-600">{{ $category->name }}</h3>
-                    </a>
+                <a href="{{ route('categories.show', $category->id) }}"
+                    class="block hover:bg-gray-400 transition duration-200 p-2 flex items-center text-decoration-none">
+                    <span class="text-lg font-semibold text-gray-700">{{ $category->name }}</span>
+                </a>
             @endforeach
         </div>
     </div>
 @endsection
+
+
+
+@extends('layouts.app')
